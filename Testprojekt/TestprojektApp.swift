a9ff3c09964cd6c76f0621e06ce87cf4cd7a300a
yevgenygordon <ygordon1978@icloud.com>
2022-12-12 12:22:19 +0100

    Initial Commit

---
+++ Testprojekt/TestprojektApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestprojektApp.swift
+//  Testprojekt
+//
+//  Created by Yevgeny Gordon on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TestprojektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
